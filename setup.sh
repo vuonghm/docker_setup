f38535cbff2323e65ac974062183fe180f5e5f27
Vuong <vuonghm@fr-s-bsg-avia-t.ncifcrf.gov>
2016-06-15 13:30:32 -0400

    Adding no prompt to setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 if [ -e "/etc/apt/sources.list.d/docker.list" ]; then
@@ -14,11 +14,11 @@ echo "Checking if it's pulling correctly"
 apt-cache policy docker-engine
 
 sudo apt-get update
-sudo apt-get install linux-image-extra-$(uname -r)
+sudo apt-get install -y linux-image-extra-$(uname -r)
 apt-get install apparmor
 
 sudo apt-get update
-sudo apt-get install docker-engine
+sudo apt-get install -y docker-engine
 sudo service docker start
 
 sudo groupadd docker
